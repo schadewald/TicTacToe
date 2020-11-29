b4378dca5141fc29f74827abfd3556dcc2832931
Chad Schadewald <ces83@students.uwf.edu>
2020-11-29 14:57:51 -0600

    Opens two windows.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,43 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class Main
+public class Main extends Application
 {
-    public static void main(String[] args) throws IOException {
-        final int PORT = 8888;
-        ServerSocket server = new ServerSocket(PORT);
-
+    GridPane gridPane = new GridPane();
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
         gameClient testClient = new gameClient();
         gameClient testClient2 = new gameClient();
         Thread thread = new Thread(testClient);
         Thread thread1 = new Thread(testClient2);
         thread.start();
         thread1.start();
+
+        Stage playerOneStage = primaryStage;
+        Stage playerTwoStage = new Stage();
+        playerOneStage.setTitle("Tic Tac Toe Player 1");
+        playerTwoStage.setTitle("Tic Tac Toe Player 2");
+        Scene mainScene = new Scene(gridPane);
+        playerOneStage.setScene(mainScene);
+        playerOneStage.setX(1000);
+        playerOneStage.setY(100);
+        playerTwoStage.setX(1000);
+        playerTwoStage.setY(500);
+        playerOneStage.show();
+        playerTwoStage.show();
+    }
+    public static void main(String[] args) throws IOException
+    {
+        final int PORT = 8888;
+        ServerSocket server = new ServerSocket(PORT);
+        launch(args);
     }
 }
--- src/sample/gameClient.java
+++ src/sample/gameClient.java
@@ -1,27 +1,13 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class gameClient extends Application implements Runnable
+public class gameClient implements Runnable
 {
-    @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        System.out.println("in start.");
-    }
     @Override
     public void run()
     {
-        GridPane gridPane = new GridPane();
         System.out.println("Game Client Called.");
-        Stage newStage = new Stage();
-        newStage.setTitle("Testing");
-        Scene mainScene = new Scene(gridPane);
-        newStage.setScene(mainScene);
-        newStage.show();
-
     }
 }
